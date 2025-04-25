de624ea63621801fad8f8e901c4e562224eccdf6
Essence Fuller <105015686+eufuller@users.noreply.github.com>
2025-04-24 22:40:14 -0700

    Fix static file delivery for admin panel

--- vb_roster/settings.py
+++ vb_roster/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -116,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/players/'
 LOGOUT_REDIRECT_URL = '/'
 
