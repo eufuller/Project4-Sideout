9fb44df8e3ecc5a257e65547d6f020b8510b43a4
Essence Fuller <105015686+eufuller@users.noreply.github.com>
2025-04-27 23:10:25 -0700

    updated settings.py to test  login redirect route "redirect path changed from '/players/' to '/' "

--- vb_roster/settings.py
+++ vb_roster/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/players/'
+# LOGIN_REDIRECT_URL = '/players/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
