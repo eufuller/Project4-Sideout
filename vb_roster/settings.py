8b38e3ea4f9971bd2aef74c150ff74106f6fa5ac
Essence Fuller <105015686+eufuller@users.noreply.github.com>
2025-04-28 16:33:52 -0700

    returned login redirect route to "/players/" in settings.py

--- vb_roster/settings.py
+++ vb_roster/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'vb_roster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# LOGIN_REDIRECT_URL = '/players/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/players/'
+#
 LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
